f83b64d83f452041d9f2b9e5f5cd9d80d0dca16c
Mateus Pinheiro <102559003+pheusss@users.noreply.github.com>
2026-03-24 17:06:49 -0300

    feat: adiciona mensagens temporizadas para os botoes de avaliacao

--- script.js
+++ script.js
@@ -5,18 +5,32 @@ let votosRuim = 0;
 let botaoBom = document.getElementById("btn-bom");
 let botaoRegular = document.getElementById("btn-regular");
 let botaoRuim = document.getElementById("btn-ruim");
+let textoAgradecimento = document.getElementById("mensagem-agradecimento");
 
 botaoBom.addEventListener("click", function(){
     votosBom = votosBom + 1;
     console.log("Alguém votou em BOM! Total: " + votosBom);
+    textoAgradecimento.innerText = "Obrigado pela sua avaliação!";
+    setTimeout(function(){
+        textoAgradecimento.innerText = "";
+    }, 3000);
+
 });
 
 botaoRegular.addEventListener("click", function(){
     votosRegular = votosRegular + 1;
     console.log("Alguém votou em REGULAR! Total: " + votosRegular);
+    textoAgradecimento.innerText = "Agradecemos o feedback! Vamos melhorar.";
+    setTimeout(function(){
+        textoAgradecimento.innerText = "";
+    }, 4000);
 });
 
 botaoRuim.addEventListener("click", function(){
     votosRuim = votosRuim + 1;
     console.log("Alguém votou em RUIM! Total: " + votosRuim);
+    textoAgradecimento.innerText = "Pedimos desculpas. Vamos resolver isso!";
+    setTimeout(function(){
+        textoAgradecimento.innerText = "";
+    }, 5000);
 })
