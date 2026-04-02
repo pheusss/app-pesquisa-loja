fb2e99366e647c84be9c7a4d5cf20171ab25ec03
Mateus Pinheiro <102559003+pheusss@users.noreply.github.com>
2026-04-02 16:25:40 -0300

    refactor: remove voting logic and update navigation functions

--- script.js
+++ script.js
@@ -1,46 +1,23 @@
-let votosBom = 0;
-let votosRegular = 0;
-let votosRuim = 0;
+const telaUm = document.getElementById("tela-csat");
+const telaDois = document.getElementById("tela-nps");
+const telaTres = document.getElementById("tela-agradecimento");
 
-let botaoBom = document.getElementById("btn-bom");
-let botaoRegular = document.getElementById("btn-regular");
-let botaoRuim = document.getElementById("btn-ruim");
-let textoAgradecimento = document.getElementById("mensagem-agradecimento");
-let textoContagemVotos = document.getElementById("total-votos");
+const blocoCarinhas = document.querySelectorAll(".btn-avaliacao");
+const botoesNps = document.querySelectorAll(".btn-nps");
 
 
-botaoBom.addEventListener("click", function(){
-    votosBom = votosBom + 1;
-    console.log("Alguém votou em BOM! Total: " + votosBom);
-    textoAgradecimento.innerText = "Obrigado pela sua avaliação!";
-    setTimeout(function(){
-        textoAgradecimento.innerText = "";
-    }, 3000);
-    atualizarPainelDoGerente();
-});
-
-botaoRegular.addEventListener("click", function(){
-    votosRegular = votosRegular + 1;
-    console.log("Alguém votou em REGULAR! Total: " + votosRegular);
-    textoAgradecimento.innerText = "Agradecemos o feedback! Vamos melhorar.";
-    setTimeout(function(){
-        textoAgradecimento.innerText = "";
-    }, 4000);
-    atualizarPainelDoGerente();
-});
-
-botaoRuim.addEventListener("click", function(){
-    votosRuim = votosRuim + 1;
-    console.log("Alguém votou em RUIM! Total: " + votosRuim);
-    textoAgradecimento.innerText = "Pedimos desculpas. Vamos resolver isso!";
-    setTimeout(function(){
-        textoAgradecimento.innerText = "";
-    }, 5000);
-    atualizarPainelDoGerente();
-});
-
-function atualizarPainelDoGerente(){
-    let totalDeVotos = votosBom + votosRegular + votosRuim;
-    textoContagemVotos.innerText = "A contagem total de votos é " + totalDeVotos;
+function avancarParaNps(){
+    telaUm.style.display = "none";
+    telaDois.style.display = "flex";
 }
 
+function finalizarAvaliacao(){
+    telaDois.style.display = "none";
+    telaTres.style.display = "flex";
+}
+
+blocoCarinhas.forEach(botao => {
+    botao.addEventListener("click", avancarParaNps)});
+
+botoesNps.forEach(botao => {
+    botao.addEventListener("click", finalizarAvaliacao)});
