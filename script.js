df9a489850238d4675ff9bcfd5a13105645118f9
Mateus Pinheiro <102559003+pheusss@users.noreply.github.com>
2026-04-20 17:24:53 -0300

    refatoração do codigo geral

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-let votosPessimo = 0;
-let votosRuim = 0;
-let votosNeutro = 0;
-let votosBom = 0;
-let votosExcelente = 0;
-
-let botaoZero = 0;
-let botaoUm = 0;
-let botaoDois = 0;
-let botaoTres = 0;
-let botaoQuatro = 0;
-let botaoCinco = 0;
-let botaoSeis = 0;
-let botaoSete = 0;
-let botaoOito = 0;
-let botaoNove = 0;
-let botaoDez = 0;
-
 const telaUm = document.getElementById("tela-csat");
 const telaDois = document.getElementById("tela-nps");
 const telaTres = document.getElementById("tela-agradecimento");
@@ -23,62 +5,24 @@ const telaTres = document.getElementById("tela-agradecimento");
 const blocoCarinhas = document.querySelectorAll(".btn-avaliacao");
 const botoesNps = document.querySelectorAll(".btn-nps");
 
+const bancoDadosCarinha = {}
+const bancoDadosNotas = {}
+
 
 function avancarParaNps(idDoVoto){
-    if(idDoVoto === "btn-pessimo"){
-        votosPessimo = votosPessimo + 1;
-    }else if(idDoVoto === "btn-ruim"){
-        votosRuim = votosRuim + 1;
-    }else if(idDoVoto === "btn-neutro"){
-        votosNeutro = votosNeutro + 1;
-    }else if(idDoVoto === "btn-bom"){
-        votosBom = votosBom + 1;
-    }else if(idDoVoto === "btn-excelente"){
-        votosExcelente = votosExcelente + 1;
-    }
+   bancoDadosCarinha[idDoVoto] += 1; 
 
-    console.log(`O cliente clicou na carinha:  ${idDoVoto} e quantidade é ${votosRuim}`);
+    console.log(`O cliente clicou na carinha: ${idDoVoto} e quantidade ${bancoDadosCarinha[idDoVoto]}`);
+    console.log("Status do Armário inteiro:", bancoDadosCarinha);
 
     telaUm.style.display = "none";
     telaDois.style.display = "flex";
 }
 
 function finalizarAvaliacao(idDaNota){
-    if(idDaNota === "btn-0"){
-        botaoZero = botaoZero + 1;
-        console.log (`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoZero}`);
-    }else if(idDaNota === "btn-1"){
-        botaoUm = botaoUm + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoUm}`);
-    }else if(idDaNota === "btn-2"){
-        botaoDois = botaoDois + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoDois}`);
-    }else if(idDaNota === "btn-3"){
-        botaoTres = botaoTres + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoTres}`);
-    }else if(idDaNota === "btn-4"){
-        botaoQuatro = botaoQuatro + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoQuatro}`);
-    }else if(idDaNota === "btn-5"){
-        botaoCinco = botaoCinco + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoCinco}`);
-    }else if(idDaNota === "btn-6"){
-        botaoSeis = botaoSeis + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoSeis}`);
-    }else if(idDaNota === "btn-7"){
-        botaoSete = botaoSete + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoSete}`);
-    }else if(idDaNota === "btn-8"){
-        botaoOito = botaoOito + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoOito}`);
-    }else if(idDaNota === "btn-9"){
-        botaoNove = botaoNove + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoNove}`);
-    }else if(idDaNota === "btn-10"){
-        botaoDez = botaoDez + 1;
-        console.log(`O cliente deu a nota: ${idDaNota} e quantidade é ${botaoDez}`);
-    }
-    
+    bancoDadosNotas[idDaNota] += 1;
+
+    console.log(`A nota dada foi ${idDaNota}`);
 
     telaDois.style.display = "none";
     telaTres.style.display = "flex";
@@ -89,12 +33,16 @@ function finalizarAvaliacao(idDaNota){
 }
 
 blocoCarinhas.forEach(botao => {
+    bancoDadosCarinha[botao.id] = 0;
+
     botao.addEventListener("click", () => {
         avancarParaNps(botao.id);
     });
 });
 
 botoesNps.forEach(botao => {
+    bancoDadosNotas[botao.id] = 0;
+
     botao.addEventListener("click", () => {
         finalizarAvaliacao(botao.id);
     });   
